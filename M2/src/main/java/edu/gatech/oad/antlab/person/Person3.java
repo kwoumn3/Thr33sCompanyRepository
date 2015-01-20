a34a179a8416aa1c14941d506f690b24bfb15ed6
tthomas45 <tthomas45@gatech.edu>
2015-01-20 18:01:35 -0500

    Reverse String
    Took the input and caused the reverse

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -8,18 +8,18 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+	/** Holds the persons real name */  
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
 	public Person3(String pname){
-	  name = pname;
+		name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
@@ -29,9 +29,9 @@ public class Person3 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
-	
+
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -42,7 +42,15 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		//Person 3 put your implementation here
+		String output = "";
+		if (input.length() <= 1) {
+			return input;  
+		}
+		for (int i = input.length(); i > 0; i--) {
+			output += input.charAt(i);
+		}
+	
+	return output;
 	}
 }
