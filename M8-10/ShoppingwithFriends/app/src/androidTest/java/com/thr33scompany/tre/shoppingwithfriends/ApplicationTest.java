9357baba1ff5a46b56f19a7dd25b1bc16f12d876
Gabriel Galarza <gabrgalarza@hotmail.com>
2015-03-29 23:28:01 -0400

    Merge Tre'Saun to Gabes local

--- M8-10/ShoppingwithFriends/app/src/androidTest/java/com/thr33scompany/tre/shoppingwithfriends/ApplicationTest.java
+++ M8-10/ShoppingwithFriends/app/src/androidTest/java/com/thr33scompany/tre/shoppingwithfriends/ApplicationTest.java
@@ -3,6 +3,8 @@ package com.thr33scompany.tre.shoppingwithfriends;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.thr33scompany.tre.shoppingwithfriends.MVP.MainActivity;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -15,4 +17,38 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public String getName() {
         return super.getName();
     }
+
+    public void checkRemoveFriend() {
+
+        MainActivity check = new MainActivity();
+        check.addFriend("Gabe");
+        check.addFriend("Frank");
+        check.addFriend("Bob");
+
+        assertEquals("GabeFrankBob", check.getFriends());
+
+        check.removeFriend("Gabe");
+        bool 
+        boolean bool = true;
+        //
+        if (check.getFriends().contains("Gabe")) {
+            bool = false;
+        }
+        assertEquals(bool, true);
+
+        if (check.getFriends().length() != "Frank".length() + "Bob".length()) {
+            bool = false;
+        }
+
+        assertEquals(bool, true);
+        assertEquals(check.getFriends(), "FrankBob");
+
+        check.removeFriend("Billy");
+        if (check.getFriends().length() != "Frank".length() + "Bob".length()) {
+            bool = false;
+        }
+        assertEquals(bool, true);
+        assert
+    }
+
 }
